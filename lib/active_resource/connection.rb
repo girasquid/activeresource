765b9f35f6993dd9fb2db7a2cb625bc2d097f9ac
Luke Hutscal <luke.hutscal@shopify.com>
2016-02-26 11:35:29 -0500

    Remove unused #request method

--- lib/active_resource/connection.rb
+++ lib/active_resource/connection.rb
@@ -168,20 +168,6 @@ module ActiveResource
     end
 
     private
-      # Makes a request to the remote service.
-      def request(method, path, *arguments)
-        result = ActiveSupport::Notifications.instrument("request.active_resource") do |payload|
-          payload[:method]      = method
-          payload[:request_uri] = "#{site.scheme}://#{site.host}:#{site.port}#{path}"
-          payload[:result]      = http.send(method, path, *arguments)
-        end
-        handle_response(result)
-      rescue Timeout::Error => e
-        raise TimeoutError.new(e.message)
-      rescue OpenSSL::SSL::SSLError => e
-        raise SSLError.new(e.message)
-      end
-
       # Performs a request to the remote service.
       def perform_request(req)
         result = ActiveSupport::Notifications.instrument("request.active_resource") do |payload|
